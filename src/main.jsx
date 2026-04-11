a6a4894be05884080a672f5a7ba5112df9d86670
Karen Cassani Patiño <karen@Karens-MacBook-Air.local>
2026-04-10 18:13:50 -0600

    Update home

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/Home.jsx'; //HOME CON MAYUSCULA
+
 import ContactMe from './pages/ContactMe.jsx';
 import Aboutme from './pages/Aboutme.jsx';
 import Projects from './pages/Projects.jsx';
